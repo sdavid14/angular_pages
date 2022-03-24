2344de4076f28bc4e4c2b070e421b9f6b7cdb77e
Stephen David <sdavid@revspringinc.com>
2022-03-24 14:28:47 -0400

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'myApp';
+export class AppComponent implements OnInit {
+  public patientEmail: any;
+  public queryParam: any;
+
+  constructor(private route: ActivatedRoute) {
+
+    this.queryParam = this.route.queryParamMap.subscribe(param => {
+    this.patientEmail = param.get('patientEmail');
+})
+
+
+  }
+
+  ngOnInit() {
+    this.route.queryParams
+    .subscribe(params=> {
+      console.log(params); // { orderby: "price"}
+    });
+
+
+
+  }
+
+  clickFunction() {
+    console.log('Hello there');
+  }
 }
